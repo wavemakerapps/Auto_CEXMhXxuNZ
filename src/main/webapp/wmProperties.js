9bd77ded708b49514bfb635dc90b54ee2877a6b8
automate qa12 <automate12@wavemaker.com>
2017-11-15 10:35:56 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_CEXMhXxuNZ",
+  "homePage" : "Main",
+  "name" : "Auto_CEXMhXxuNZ",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
